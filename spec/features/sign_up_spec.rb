e9c1566c093e247cff66d781abc80361c97cd446
Riya Dattani <riya_dattani@hotmail.com>
2019-04-08 17:40:55 +0100

    Create sign up feature
    Create sign up form on homepage
    Create homepage and spaces views

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,13 @@
+feature 'Sign up to Makersbnb' do
+  scenario 'user fills a form with their details' do
+    visit '/homepage'
+    fill_in('fullname', with: 'Riya Dattani')
+    fill_in('email', with: 'test@example.com')
+    fill_in('password', with: 'password123')
+
+    click_button 'Submit'
+    expect(page).to have_current_path('/spaces')
+    expect(page).to have_content 'Welcome Riya Dattani'
+    expect(page).to have_content 'Book a Space'
+  end
+end
