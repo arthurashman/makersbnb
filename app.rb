320365e22b41d7002044102865af25bc5448d003
Riya Dattani <riya_dattani@hotmail.com>
2019-04-09 15:07:43 +0100

    existing user cannot sign up again

--- app.rb
+++ app.rb
@@ -14,9 +14,14 @@ class Makersbnb < Sinatra::Base
   end
 
   post '/sign_up' do
-    user = User.create(fullname: params[:fullname], email: params[:email], password: params[:password])
-    session[:email] = user.email
-    redirect '/spaces'
+    if User.find(email: params[:email])
+      flash[:notice] = 'You have an account, please log in.'
+      redirect('/log_in')
+    else
+      user = User.create(fullname: params[:fullname], email: params[:email], password: params[:password])
+      session[:email] = user.email
+      redirect '/spaces'
+    end
   end
 
   get '/spaces' do
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -10,4 +10,15 @@ feature 'Sign up to Makersbnb' do
     expect(page).to have_content 'Welcome Riya Dattani'
     expect(page).to have_content 'Book a Space'
   end
+
+  scenario 'if user exists, redirect to log in page' do
+    User.create(fullname: 'Riya Dattani', email: 'test@example.com', password: 'password123')
+    visit '/'
+    fill_in('fullname', with: 'Riya Dattani')
+    fill_in('email', with: 'test@example.com')
+    fill_in('password', with: 'password123')
+
+    click_button 'Sign up'
+    expect(page).to have_content 'You have an account, please log in.'
+  end
 end
