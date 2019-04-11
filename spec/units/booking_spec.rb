7278c917a561e646d994ea5e7b16d50e926fa08d
kimdiep <kim.diep.techno@gmail.com>
2019-04-11 12:17:49 +0100

    requester_id added to bookings table readme updated sql script added

--- spec/units/booking_spec.rb
+++ spec/units/booking_spec.rb
@@ -1,12 +1,14 @@
 require 'database_helpers'
 require 'booking'
+require 'user'
+require 'space'
 
 
 describe Booking do
   describe '.request' do
 
     it 'creates a new booking' do
-      booking = Booking.request(space_id: 1, date:'2019-05-20', confirmation: "false")
+      booking = Booking.request(requester_id: 1, space_id: 1, date:'2019-05-20', confirmation: "false")
       persisted_data = persisted_data(table: :bookings, id: booking.id)
 
       expect(booking).to be_a Booking
@@ -17,13 +19,15 @@ describe Booking do
 
   describe '#available?' do
     it 'checks if the space is available' do
-      booking = Booking.request(space_id: 1, date:'2019-05-20', confirmation: "false")
+      booking = Booking.request(requester_id: 1, space_id: 1, date:'2019-05-20', confirmation: "false")
       expect(booking.available?(space_id: booking.space_id, date:'2019-05-20')).to be true
     end
 
     it 'checks if the space is available' do
-      booking = Booking.request(space_id: 1, date:'2019-05-20', confirmation: "true")
+      booking = Booking.request(requester_id: 1, space_id: 1, date:'2019-05-20', confirmation: "true")
       expect(booking.available?(space_id: booking.space_id, date:'2019-05-20')).to be false
     end
   end
+
+
 end
