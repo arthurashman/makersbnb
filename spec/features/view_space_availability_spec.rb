986982da679f8de1fa0ce34913a15ebf77c79f16
Riya Dattani <riya_dattani@hotmail.com>
2019-04-12 14:41:56 +0100

    requester cant book a confirmed booking at a particular date

--- spec/features/view_space_availability_spec.rb
+++ spec/features/view_space_availability_spec.rb
@@ -42,4 +42,38 @@ feature 'View the availability of a space' do
     expect(page).to have_current_path("/spaces")
   end
 
+
+  scenario 'requester cannot book an unavailable space' do
+    owner = User.create(fullname: 'Riya Dattani', email: 'test@example.com', password: 'password123')
+    Space.create(user_id: owner.id, title: "Beautiful Home", description: "Beautiful home in Yorkshire", price_per_night: 50, date_from: "2019-05-01", date_to: "2019-05-31")
+    requester = User.create(fullname: 'Yoda', email: 'yoda@example.com', password: 'password123')
+    requester2 = User.create(fullname: 'Yoda2', email: 'yoda2@example.com', password: 'password123')
+    visit '/log_in'
+    fill_in('email', with: 'yoda@example.com')
+    fill_in('password', with: 'password123')
+    click_button('Log in')
+    click_button('See more')
+    fill_in(:chosen_date, with: "2019-05-20")
+    click_button('Request')
+    click_button('Log out')
+
+    visit '/log_in'
+    fill_in('email', with: 'test@example.com')
+    fill_in('password', with: 'password123')
+    click_button('Log in')
+    click_button('My requests')
+    click_button('Confirm')
+    click_button('Log out')
+
+    visit '/log_in'
+    fill_in('email', with: 'yoda2@example.com')
+    fill_in('password', with: 'password123')
+    click_button('Log in')
+    click_button('See more')
+    fill_in(:chosen_date, with: "2019-05-20")
+    click_button('Request')
+
+    expect(page).to have_content('Sorry this date is unavailable')
+  end
+
 end
